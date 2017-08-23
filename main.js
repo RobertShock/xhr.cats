e8494247f2e8997e602f4942c69fc7be7dfcf707
Robbie Shock <rob.f.shock@gmail.com>
2017-08-22 20:15:43 -0500

    prints to dom

---
+++ main.js
@@ -0,0 +1,45 @@
+var cats = [
+{name: "Fluffy", color: "Green", url:"https://i.ytimg.com/vi/-OJMIqVrON0/maxresdefault.jpg"},
+{name: "Krabby", color: "Red", url:"https://thechive.files.wordpress.com/2016/10/1874295-211.jpg?quality=85&strip=info&w=600"},
+{name: "Maximus", color: "N/A", url:"https://pbs.twimg.com/media/CVB4sEnUsAAQaCN.jpg"}
+];
+
+function domString(){
+}
+
+function writeToDom(){
+}
+
+var catArray = [];
+
+domString(cats);
+
+var catContainer = document.getElementById("cat-container");
+
+function printCatArrayToDom(array) {
+ 	for ( var i = 0; i < array.length; i++ ) {
+ 		var currentCat = array[i];
+ 		var catDomString = catDomStringFunction(currentCat);
+ 	catContainer.innerHTML += catDomString;
+}
+}
+
+function catDomStringFunction(currentCat) {
+  var catDomString = "";
+ 		catDomString += '<section class="cat">';
+ 		catDomString += 	'<div class="catImage">';
+ 		catDomString += 		'<img src="' + currentCat.url + '">';
+ 		catDomString += 	'</div>';
+ 		catDomString += 	'<div class="catName">';
+ 		catDomString += 		'<h2>' + currentCat.name + '</h2>';
+ 		catDomString +=		'</div>';
+ 		catDomString += 	'<div class="catColor">'
+ 		catDomString += 		'<p>' + currentCat.color + '</p>';
+ 		catDomString += 	'</div>'
+ 		catDomString +=		'</section>';
+ 		return catDomString;
+}
+
+printCatArrayToDom(cats);
+
+
